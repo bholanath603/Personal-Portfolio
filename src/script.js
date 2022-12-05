fd2ee0c530691763e8ac488549f48c7ac0dd81db
bholanath603 <104128556+bholanath603@users.noreply.github.com>
2022-12-06 01:55:32 +0530

    push5

--- src/script.js
+++ src/script.js
@@ -10,3 +10,31 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+//----sidemenu-----//
+var sidemenu=document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right="0";
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+
+//-----contact------//
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxKSTOFuQ91Nl6yK0d53FVa4FZS2NgHMa1KRZNtKSTcVqIh8HUQho-_0QWPpe1VkeAV/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg=document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML="Submitted Sucessfully"
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
